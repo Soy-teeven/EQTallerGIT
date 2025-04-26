438a6e77f54041a3de116a339ea96c6f37eb06f2
Soy-teeven <soysteevenquishpi@gmail.com>
2025-04-26 15:06:19 -0500

    Commit inicial

---
+++ src/main/java/com/mycompany/eqtallergit/EQTallerGIT.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eqtallergit;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author USER
+ */
+public class EQTallerGIT {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Cual es tu nombre?");
+        String nombre=scanner.nextLine();
+        System.out.println("Hola "+nombre+"!");
+    }
+}
