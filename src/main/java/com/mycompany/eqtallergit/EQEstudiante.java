27a2ff095f1457e0df449dd05c22d7592405a436
Soy-teeven <soysteevenquishpi@gmail.com>
2025-04-26 15:46:02 -0500

    Primer cambio

---
+++ src/main/java/com/mycompany/eqtallergit/EQEstudiante.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.eqtallergit;
+
+/**
+ *
+ * @author USER
+ */
+public class EQEstudiante {
+    //primer cambio realizado segun lo pedido en el taller
+}
