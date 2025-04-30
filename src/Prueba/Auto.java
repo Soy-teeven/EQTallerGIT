88a23b8d2701c24e02df74759050fda0dc56009c
joeltapia <jatapiasuarez49@gmail.com>
2025-04-30 12:04:16 -0500

    Primer cambio en clase

---
+++ src/Prueba/Auto.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Prueba;
+
+/**
+ *
+ * @author LABS-ESPE
+ */
+public class Auto {
+    //Crear atributos
+    String marca;
+    String modelo;
+    int precio;
+    //Metodos de la clase Auto
+    public void encender(){
+        System.out.println("El auto esta encendido");
+    }
+    public void apagar(){
+        System.out.println("El auto esta apagado");
+    }
+    public void acelerar(){
+        System.out.println("El auto esta acelerando");
+    }
+    public void frenar(){
+        System.out.println("El auto esta frenando");
+    }
+}
---
+++ src/Prueba/Principal.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Prueba;
+
+/**
+ *
+ * @author LABS-ESPE
+ */
+public class Principal {
+   //Clase principal 
+    public static void main(String[] args) {
+        //Crear un objeto para llamar a un metodo de una clase
+        //nombreClase cualquierNombre= new nombreClase()
+        Auto llamada=new Auto();
+        System.out.println("Hola Mundo");
+        llamada.acelerar();
+        llamada.apagar();
+    }
+    
+}
